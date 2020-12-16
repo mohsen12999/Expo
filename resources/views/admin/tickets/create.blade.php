d6c098b79ad3964c64b2c11e6882aaa001e73885
mohsen <mohsen1299@gmail.com>
2020-12-16 22:26:02 +0330

    correct ticket view

--- resources/views/admin/tickets/create.blade.php
+++ resources/views/admin/tickets/create.blade.php
@@ -7,7 +7,7 @@
 
 @section('content')
 <section>
-    <h1>{{ __('words.CreateTicket') }}</h1>
+  <h1>{{ __('words.CreateTicket') }}</h1>
 </section>
 
 <section class="content container-fluid">
@@ -15,58 +15,60 @@
 | Your Page Content Here |
 -------------------------->
 
-<div class="col-xs-12">
-  <div class="box box-primary">
-      
-    <div class="box-header with-border">
-      <h3 class="box-title">{{ __('words.CreateTicket') }}</h3>
-    </div>
+  <div class="col-xs-12">
+    <div class="box box-primary">
 
-    <form role="form" method="post"  action="{{ url('admin/ticket/') }}" enctype="multipart/form-data">
-      @csrf
+      <div class="box-header with-border">
+        <h3 class="box-title">{{ __('words.CreateTicket') }}</h3>
+      </div>
 
-      <div class="box-body">
-        <div asp-validation-summary="ModelOnly" class="text-danger"></div>
+      <form role="form" method="post" action="{{ url('admin/ticket/') }}" enctype="multipart/form-data">
+        @csrf
 
-        <div class="form-group">
-          <label for="type" class="control-label">{{ __('words.TicketSubject') }}</label>
-          <select id="type" name="type" class="form-control">
-                <option value="0">{{ __('words.Other') }}</option>
-                <option value="1">{{ __('words.Technical') }}</option>
-                <option value="2">{{ __('words.Financial') }}</option>
-          </select>
-          <span for="type" class="text-danger"></span>
-        </div>
+        <div class="box-body">
+          <div asp-validation-summary="ModelOnly" class="text-danger"></div>
 
-        <div class="form-group">
+          <div class="form-group">
+            <label for="type" class="control-label">{{ __('words.TicketSubject') }}</label>
+            <select id="type" name="type" class="form-control">
+              <option value="0">{{ __('words.Other') }}</option>
+              <option value="1">{{ __('words.Technical') }}</option>
+              <option value="2">{{ __('words.Financial') }}</option>
+            </select>
+            <span for="type" class="text-danger"></span>
+          </div>
+
+          <div class="form-group">
             <label for="title" class="control-label">{{ __('words.Title') }}</label>
-            <input id="title" name="title" class="form-control"/>
+            <input id="title" name="title" class="form-control" required />
             <span for="title" class="text-danger"></span>
+          </div>
+
+          <div class="form-group">
+            <label for="description" class="control-label">{{ __('words.Description') }}</label>
+            <textarea id="description" name="description" class="form-control"
+              placeholder="{{ __('words.Description') }} ..." required></textarea>
+            <span for="description" class="text-danger"></span>
+          </div>
+
+          <div class="form-group">
+            <label for="attachment" class="control-label">{{ __('words.Attachment') }}</label>
+            <input id="attachment" name="attachment" type="file" accept="image/*,video/*,text/plain,application/pdf"
+              class="form-control-file" />
+            <span for="attachment" class="text-danger"></span>
+          </div>
+
         </div>
 
-        <div class="form-group">
-          <label for="description" class="control-label">{{ __('words.Description') }}</label>
-          <textarea id="description" name="description" class="form-control" placeholder="{{ __('words.Description') }} ..." ></textarea>
-          <span for="description" class="text-danger"></span>
-        </div>
-
-        <div class="form-group">
-          <label for="attachment" class="control-label">{{ __('words.Attachment') }}</label>
-          <input id="attachment" name="attachment" type="file" accept="image/*,video/*,text/plain,application/pdf" class="form-control-file"/>
-          <span for="attachment" class="text-danger"></span>
-        </div>
-
-      </div>
-
-      <!-- /.box-body -->
-      <div class="box-footer">
+        <!-- /.box-body -->
+        <div class="box-footer">
           <button type="submit" class="btn btn-primary"><i class="fa fa-save"></i> {{ __('words.SendTicket') }}</button>
-      </div>
+        </div>
 
-    </form>
+      </form>
 
+    </div>
   </div>
-</div>
 
 </section>
 
--- resources/views/admin/tickets/index.blade.php
+++ resources/views/admin/tickets/index.blade.php
@@ -7,7 +7,7 @@
 
 @section('content')
 <section>
-    <h1>{{ __('words.YourTicketsList') }}</h1>
+  <h1>{{ __('words.YourTicketsList') }}</h1>
 </section>
 
 <section class="content container-fluid">
@@ -15,69 +15,75 @@
 | Your Page Content Here |
 -------------------------->
 
-<div class="col-xs-12">
-  <div class="box">
+  <div class="col-xs-12">
+    <div class="box">
 
-    <!-- box-header -->
-    <div class="box-header">
-      <h3 class="box-title">{{ __('words.YourTicketsList') }}</h3>
-      <a href="{{url('admin\ticket\create')}}" class="btn btn-primary pull-right" style="margin-left: 1em;"><i class="fa fa-plus"></i> {{ __('words.NewTicket') }}</a>
-    </div>
-    <!-- /.box-header -->
+      <!-- box-header -->
+      <div class="box-header">
+        <h3 class="box-title">{{ __('words.YourTicketsList') }}</h3>
+        <a href="{{url('admin\ticket\create')}}" class="btn btn-primary pull-right" style="margin-left: 1em;"><i
+            class="fa fa-plus"></i> {{ __('words.NewTicket') }}</a>
+      </div>
+      <!-- /.box-header -->
 
 
-    <div class="box-body table-responsive no-padding">
+      <div class="box-body table-responsive no-padding">
 
-      @if (count($tickets) === 0)
+        @if (count($tickets) === 0)
         <div class="no-data">
           <h3>{{ __('words.NoTicketMessage') }}</h3>
-          <a href="{{url('admin\ticket\create')}}" class="btn btn-primary" style="margin-left: 1em;"><i class="fa fa-plus"></i> {{ __('words.NewTicket') }}</a>
+          <a href="{{url('admin\ticket\create')}}" class="btn btn-primary" style="margin-left: 1em;"><i
+              class="fa fa-plus"></i> {{ __('words.NewTicket') }}</a>
         </div>
-      @else
-          
-      <table class="table table-hover persian-table table-striped table-bordered">
-        <tr>
-          <th>{{ __('words.TicketNo') }}</th>
-          <th>{{ __('words.Title') }}</th>
-          <th>{{ __('words.Subject') }}</th>
-          <th>{{ __('words.Status') }}</th>
-          <th>{{ __('words.Functions') }}</th>
-        </tr>
+        @else
 
-        @foreach ($tickets as $ticket)
+        <table class="table table-hover persian-table table-striped table-bordered">
+          <tr>
+            <th>{{ __('words.TicketNo') }}</th>
+            <th>{{ __('words.Title') }}</th>
+            <th>{{ __('words.Subject') }}</th>
+            <th>{{ __('words.Status') }}</th>
+            <th>{{ __('words.Functions') }}</th>
+          </tr>
 
-        <tr>
-          <td>{{ $ticket->id }}</td>
-          <td>{{ $ticket->title }}</td>
-          <td>{{ $ticket->type_string() }}</td>
-          <td>{{ $ticket->status_string() }}</td>
+          @foreach ($tickets as $ticket)
 
-          <td>
-            <a class="btn btn-warning" href="{{action('Admin\TicketController@edit', $ticket['id'])}}">
-              <i class="fa fa-edit" title="{{ __('words.SeeTicket') }}"></i> {{ __('words.SeeTicket') }}
-            </a>
-            <form style="display:inline;" action="{{ url('admin/ticket/' . $ticket->id) }}" method="post"
-              onsubmit="return confirm('{{ __('words.CloseTicketMessage') }}');">
-                  @csrf
-                  @method('DELETE')
-                  <button class="btn btn-danger" title="{{ __('words.CloseTicket') }}">
-                      <i class="fa fa-trash-o" title="{{ __('words.CloseTicket') }}"></i> {{ __('words.CloseTicket') }}
-                  </button>
-            </form>
+          <tr>
+            <td>{{ $ticket->id }}</td>
+            <td>{{ $ticket->title }}</td>
+            <td>{{ $ticket->type_string() }}</td>
+            <td>{{ $ticket->status_string() }}</td>
 
-          </td>
-        </tr>
+            <td>
+              <a class="btn btn-warning" href="{{action('Admin\TicketController@edit', $ticket['id'])}}">
+                <i class="fa fa-edit" title="{{ __('words.SeeTicket') }}"></i> {{ __('words.SeeTicket') }}
+              </a>
 
-        @endforeach
+              @if ($ticket->status != 3)
+              <form style="display:inline;" action="{{ url('admin/ticket/' . $ticket->id) }}" method="post"
+                onsubmit="return confirm('{{ __('words.CloseTicketMessage') }}');">
+                @csrf
+                @method('DELETE')
+                <button class="btn btn-danger" title="{{ __('words.CloseTicket') }}">
+                  <i class="fa fa-trash-o" title="{{ __('words.CloseTicket') }}"></i> {{ __('words.CloseTicket') }}
+                </button>
+              </form>
+              @endif
 
-      </table>
 
-      @endif
+            </td>
+          </tr>
+
+          @endforeach
+
+        </table>
+
+        @endif
+
+      </div>
 
     </div>
-
   </div>
-</div>
 
 </section>
 
