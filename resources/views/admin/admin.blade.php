c22736e6faa71856873266a2b8444f83401c5a9b
mohsen <mohsen1299@gmail.com>
2020-12-16 22:22:09 +0330

    add ticket in dashboard

--- resources/views/admin/admin.blade.php
+++ resources/views/admin/admin.blade.php
@@ -7,7 +7,7 @@
 
 @section('content')
 <section class="content-header">
-    <h1>{{ __('words.Dashboard') }}</h1>
+  <h1>{{ __('words.Dashboard') }}</h1>
 </section>
 
 <section class="content">
@@ -24,7 +24,8 @@
         <div class="icon">
           <i class="fa fa-image"></i>
         </div>
-        <a href="{{url('admin\expo')}}" class="small-box-footer">{{ __('words.ExpoList') }} <i class="fa fa-arrow-circle-right"></i></a>
+        <a href="{{url('admin\expo')}}" class="small-box-footer">{{ __('words.ExpoList') }} <i
+            class="fa fa-arrow-circle-right"></i></a>
       </div>
     </div>
 
@@ -39,7 +40,8 @@
         <div class="icon">
           <i class="fa fa-list"></i>
         </div>
-        <a href="{{url('admin\category')}}" class="small-box-footer">{{ __('words.CategoryList') }} <i class="fa fa-arrow-circle-right"></i></a>
+        <a href="{{url('admin\category')}}" class="small-box-footer">{{ __('words.CategoryList') }} <i
+            class="fa fa-arrow-circle-right"></i></a>
       </div>
     </div>
 
@@ -54,39 +56,58 @@
         <div class="icon">
           <i class="fa fa-shopping-cart"></i>
         </div>
-        <a href="{{url('admin\package')}}" class="small-box-footer">{{ __('words.PackageList') }} <i class="fa fa-arrow-circle-right"></i></a>
+        <a href="{{url('admin\package')}}" class="small-box-footer">{{ __('words.PackageList') }} <i
+            class="fa fa-arrow-circle-right"></i></a>
       </div>
     </div>
 
     <div class="col-lg-4 col-xs-6">
-        <!-- small box -->
-        <div class="small-box bg-red">
-          <div class="inner">
-            <h3>{{$User_count}}</h3>
+      <!-- small box -->
+      <div class="small-box bg-red">
+        <div class="inner">
+          <h3>{{$User_count}}</h3>
 
-            <p>{{ __('words.Users') }}</p>
-          </div>
-          <div class="icon">
-            <i class="fa fa-user"></i>
-          </div>
-          <a href="{{url('admin\users')}}" class="small-box-footer">{{ __('words.UserList') }} <i class="fa fa-arrow-circle-right"></i></a>
+          <p>{{ __('words.Users') }}</p>
         </div>
-      </div>
-
-      <div class="col-lg-4 col-xs-6">
-        <!-- small box -->
-        <div class="small-box bg-green">
-          <div class="inner">
-            <h3>{{$invoice_count}}</h3>
-
-            <p>{{ __('words.Invoices') }}</p>
-          </div>
-          <div class="icon">
-            <i class="fa fa-object-ungroup"></i>
-          </div>
-          <a href="{{url('admin\invoices')}}" class="small-box-footer">{{ __('words.InvoiceList') }} <i class="fa fa-arrow-circle-right"></i></a>
+        <div class="icon">
+          <i class="fa fa-user"></i>
         </div>
+        <a href="{{url('admin\users')}}" class="small-box-footer">{{ __('words.UserList') }} <i
+            class="fa fa-arrow-circle-right"></i></a>
       </div>
+    </div>
+
+    <div class="col-lg-4 col-xs-6">
+      <!-- small box -->
+      <div class="small-box bg-green">
+        <div class="inner">
+          <h3>{{$invoice_count}}</h3>
+
+          <p>{{ __('words.Invoices') }}</p>
+        </div>
+        <div class="icon">
+          <i class="fa fa-object-ungroup"></i>
+        </div>
+        <a href="{{url('admin\invoices')}}" class="small-box-footer">{{ __('words.InvoiceList') }} <i
+            class="fa fa-arrow-circle-right"></i></a>
+      </div>
+    </div>
+
+    <div class="col-lg-4 col-xs-6">
+      <!-- small box -->
+      <div class="small-box bg-purple">
+        <div class="inner">
+          <h3>{{$ticket_count}}</h3>
+
+          <p>{{ __('words.Tickets') }}</p>
+        </div>
+        <div class="icon">
+          <i class="fa fa-comments"></i>
+        </div>
+        <a href="{{url('admin\tickets')}}" class="small-box-footer">{{ __('words.TicketsList') }} <i
+            class="fa fa-arrow-circle-right"></i></a>
+      </div>
+    </div>
 
   </div>
 </section>
