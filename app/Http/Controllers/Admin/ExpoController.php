e7805e8297fdacc04e83a8c3bfdf2d1192814237
mohsen <mohsen1299@gmail.com>
2020-12-16 22:17:14 +0330

    admin functions for expos

--- app/Http/Controllers/Admin/ExpoController.php
+++ app/Http/Controllers/Admin/ExpoController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Booth;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Intervention\Image\ImageManagerStatic;
@@ -35,7 +36,7 @@ class ExpoController extends Controller
         //
         // $courses = Course::all();
         $menu = 'expo';
-        $expos = Expo::with(['expoComments', 'expoImages'])->orderBy('id', 'desc')->get();
+        $expos = Expo::with(['booths', 'expoComments', 'expoImages'])->orderBy('id', 'desc')->get();
 
         return view('admin.expos.index', compact('expos', 'menu'));
     }
@@ -263,5 +264,43 @@ class ExpoController extends Controller
         $image->delete();
 
         return redirect('admin/expo-image/' . $expo_id)->with('success', 'Information has been Removed');
+    }
+
+    public function expoAdmin($id)
+    {
+        $menu = 'expo';
+        $booths = Booth::where('expo_id', $id)->get();
+        $expo = Expo::find($id);
+        return view('admin.expos.admin', compact('booths', 'menu', 'expo'));
+    }
+
+    public function changeBoothLoaction($id)
+    {
+        $menu = 'expo';
+        $booth = Booth::find($id);
+        $booth_numbers = ["101", "102", "103", "104", "105", "106", "107", "108", "109", "201", "301", "401", "227", "327", "427", "202", "204", "206", "208", "220", "222", "224", "224", "226", "504", "506", "508", "510", "512", "514", "516", "518", "520", "522", "524", "526", "403", "405", "407", "409", "411", "413", "415", "417", "423", "425", "404", "406", "408", "410", "412", "414", "416", "418", "420", "422", "424", "426", "303", "305", "307", "309", "311", "313", "315", "317", "319", "319", "321", "323", "325", "304", "306", "308", "310", "312", "318", "322", "324", "326", "203", "205", "207", "209", "219", "221", "223", "225"];
+        $other_booths = Booth::with('user')->where('expo_id', $booth->expo_id)->get();
+        $other_booths_array = $other_booths->toArray();
+
+        return view('admin.expos.booth_location', compact('booth', 'menu', 'booth_numbers', 'other_booths', 'other_booths_array'));
+    }
+
+    public function changingBoothLoaction(Request $request, $map_id)
+    {
+        $id = $request->id;
+        $booth = Booth::find($id);
+        $booth->booth_map_id = $map_id;
+        $booth->save();
+
+        return redirect('admin/expo-admin/' . $booth->expo_id)->with('success', 'Booth location Changed');
+    }
+
+
+    public function boothConfirm(Request $request, $id)
+    {
+        $booth = Booth::find($id);
+        $booth->confirm = 1;
+        $booth->save();
+        return redirect('admin/expo-admin/' . $booth->expo_id)->with('success', 'Booth Confirmed');
     }
 }
