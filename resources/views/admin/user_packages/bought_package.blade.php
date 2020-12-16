9281fe12296f027e5d45ac7593e625c7ce11a3ab
mohsen <mohsen1299@gmail.com>
2020-12-16 22:23:40 +0330

    currect url in bought package page

--- resources/views/admin/user_packages/bought_package.blade.php
+++ resources/views/admin/user_packages/bought_package.blade.php
@@ -7,33 +7,38 @@
 
 @section('content')
 <section>
-    <h1>{{ __('words.BoughtPackage') }}</h1>
+  <h1>{{ __('words.BoughtPackage') }}</h1>
 </section>
 
 <section class="content container-fluid">
   <!--------------------------
 | Your Page Content Here |
 -------------------------->
-<div class="col-xs-12">
-  <div class="box">
+  <div class="col-xs-12">
+    <div class="box">
 
-    <!-- box-header -->
-    <div class="box-header">
-      <h3 class="box-title">{{ __('words.BoughtPackageInfo') }}</h3>
-    </div>
-    <!-- /.box-header -->
-
-    <div class="box-body table-responsive no-padding">
-      <div class="no-data">
-        <h3>{{ __('words.BoughtPackageInfo') }}</h3>
-        <p>{{ __('words.PackageNumber') }}: {{ $userPackage->id }}</p>
-        <p>{{ __('words.PackageBoughtTime') }}: {{ $userPackage->created_at }}</p>
-        <a href="{{url('admin/user-package')}}" class="btn btn-info" style="margin-left: 1em;"><i class="fa fa-list"></i> See Your Packages</a> <a href="{{url('admin\category')}}" class="btn btn-primary" style="margin-left: 1em;"><i class="fa fa-plus"></i> Buy new Category </a>
+      <!-- box-header -->
+      <div class="box-header">
+        <h3 class="box-title">{{ __('words.BoughtPackageInfo') }}</h3>
       </div>
-    </div>
+      <!-- /.box-header -->
 
+      <div class="box-body table-responsive no-padding">
+        <div class="no-data">
+          <h3>{{ __('words.BoughtPackageInfo') }}</h3>
+          <p>{{ __('words.PackageNumber') }}: {{ $userPackage->id }}</p>
+          <p>{{ __('words.PackageBoughtTime') }}: {{ $userPackage->created_at }}</p>
+          <a href="{{url('admin/user-package')}}" class="btn btn-info" style="margin-left: 1em;"><i
+              class="fa fa-list"></i> See Your Packages
+          </a>
+          <a href="{{url('admin/user-category/choose-package')}}" class="btn btn-primary" style="margin-left: 1em;"><i
+              class="fa fa-plus"></i> Buy new Category
+          </a>
+        </div>
+      </div>
+
+    </div>
   </div>
-</div>
 
 
 </section>
