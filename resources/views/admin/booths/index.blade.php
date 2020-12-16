bc9b2ffe931af807e5137d1f0aaff25a3f081c41
mohsen <mohsen1299@gmail.com>
2020-12-16 22:26:51 +0330

    add confirm colomn for booth

--- resources/views/admin/booths/index.blade.php
+++ resources/views/admin/booths/index.blade.php
@@ -7,7 +7,7 @@
 
 @section('content')
 <section>
-    <h1>{{ __('words.YourBooths') }}</h1>
+  <h1>{{ __('words.YourBooths') }}</h1>
 </section>
 
 <section class="content container-fluid">
@@ -17,14 +17,16 @@
 
       <div class="box-header with-border">
         <h3 class="box-title">{{ __('words.YourBooths') }}</h3>
-        <a href="{{url('admin\booth\create')}}" class="btn btn-primary pull-right" style="margin-left: 1em;"><i class="fa fa-plus"></i> {{ __('words.NewBooth') }}</a>
+        <a href="{{url('admin\booth\create')}}" class="btn btn-primary pull-right" style="margin-left: 1em;"><i
+            class="fa fa-plus"></i> {{ __('words.NewBooth') }}</a>
       </div>
 
       @if(count($booths) === 0)
-        <div class="no-data">
-          <h3>{{ __('words.NoBoothsMessage') }}</h3>
-          <a href="{{url('admin\booth\create')}}" class="btn btn-primary" style="margin-left: 1em;"><i class="fa fa-plus"></i> {{ __('words.NewBooth') }}</a>
-        </div>
+      <div class="no-data">
+        <h3>{{ __('words.NoBoothsMessage') }}</h3>
+        <a href="{{url('admin\booth\create')}}" class="btn btn-primary" style="margin-left: 1em;"><i
+            class="fa fa-plus"></i> {{ __('words.NewBooth') }}</a>
+      </div>
       @else
 
       <table class="table table-hover persian-table table-striped table-bordered">
@@ -33,34 +35,36 @@
           <th>{{ __('words.Description') }}</th>
           <th>{{ __('words.Expo') }}</th>
           <th>{{ __('words.Status') }}</th>
+          <th>{{ __('words.Confirmed') }}</th>
           <th>{{ __('words.Functions') }}</th>
         </tr>
 
         @foreach ($booths as $booth)
 
-          <tr>
-            <td> {{ $booth->title }} </td>
-            <td> {{ $booth->description }} </td>
+        <tr>
+          <td> {{ $booth->title }} </td>
+          <td> {{ $booth->description }} </td>
 
-            <td> {{ $booth->expo->title }} </td>
-            <td> {{ $booth->status_string() }} </td>
+          <td> {{ $booth->expo->title }} </td>
+          <td> {{ $booth->status_string() }} </td>
+          <td> {{ $booth->confirm==0?__('words.Pending'):__('words.Accepted') }} </td>
 
-            <td>
-              <a class="btn btn-warning" href="{{action('Admin\BoothController@edit', $booth['id'])}}">
-                <i class="fa fa-edit" title="{{ __('words.ChangeBooth') }}"></i> {{ __('words.ChangeBooth') }}
-              </a>
-              <form style="display:inline;" action="{{ url('admin/booth/' . $booth->id) }}" method="post"
-                onsubmit="return confirm('{{ __('words.DeleteBoothMessage') }}');">
-                    @csrf
-                    @method('DELETE')
-                    <button class="btn btn-danger" title="{{ __('words.DeleteBooth') }}">
-                        <i class="fa fa-trash-o" title="{{ __('words.DeleteBooth') }} "></i> {{ __('words.DeleteBooth') }} 
-                    </button>
-              </form>
+          <td>
+            <a class="btn btn-warning" href="{{action('Admin\BoothController@edit', $booth['id'])}}">
+              <i class="fa fa-edit" title="{{ __('words.ChangeBooth') }}"></i> {{ __('words.ChangeBooth') }}
+            </a>
+            <form style="display:inline;" action="{{ url('admin/booth/' . $booth->id) }}" method="post"
+              onsubmit="return confirm('{{ __('words.DeleteBoothMessage') }}');">
+              @csrf
+              @method('DELETE')
+              <button class="btn btn-danger" title="{{ __('words.DeleteBooth') }}">
+                <i class="fa fa-trash-o" title="{{ __('words.DeleteBooth') }} "></i> {{ __('words.DeleteBooth') }}
+              </button>
+            </form>
 
-            </td>
-          </tr>
-          
+          </td>
+        </tr>
+
         @endforeach
 
       </table>
@@ -71,20 +75,21 @@
         <div class="col-md-3 col-xs-6">
           <div class="mycard">
             <img class="mycard-img" src="{{ $booth->pic }}" />
-            <h4>{{ $booth->title }}</h4>
-          </div>
-
-        </div>
-        @endforeach --}}
-
+        <h4>{{ $booth->title }}</h4>
       </div>
 
-      <div class="no-data">
-        <a href="{{url('admin\booth\create')}}" class="btn btn-primary" style="margin-left: 1em;"><i class="fa fa-plus"></i> new Booth</a>
-      </div>
-
-      @endif
     </div>
+    @endforeach --}}
+
+  </div>
+
+  <div class="no-data">
+    <a href="{{url('admin\booth\create')}}" class="btn btn-primary" style="margin-left: 1em;"><i class="fa fa-plus"></i>
+      new Booth</a>
+  </div>
+
+  @endif
+  </div>
   </div>
 </section>
 
