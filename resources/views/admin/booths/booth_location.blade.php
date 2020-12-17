b38499cbaa98170501b96460bf7b6ca5a3d3bd5b
mohsen <mohsen1299@gmail.com>
2020-12-17 21:14:42 +0330

    little fix in booths views

--- resources/views/admin/booths/booth_location.blade.php
+++ resources/views/admin/booths/booth_location.blade.php
@@ -15,20 +15,23 @@
 
     <form action="" method="post">
         @csrf
-        <input name="id" id="id" type="hidden" value="{{$booth_id}}"/>
+        <input name="id" id="id" type="hidden" value="{{$booth_id}}" />
 
         <div class="expo-map col-12">
-            <img src="{{ asset("/img/expo-map.jpg") }}" style="width: 100%" alt="expo-map"/>
+            <img src="{{ asset("/img/expo-map.jpg") }}" style="width: 100%" alt="expo-map" />
 
             @foreach ($booth_numbers as $booth_number)
-                @if (in_array($booth_number,array_column($other_booths_array,'booth_map_id')))
-                    @php
-                        $this_booth = $other_booths->where("booth_map_id","=",$booth_numbers)->first();
-                    @endphp
-                    <button type="button" disabled class="btn btn-secondary btn-{{$booth_number}}" title="{{$this_booth->user->name}}">{{$booth_number}}</button>
-                @else
-                    <button type="submit" formaction="/admin/booth/booth-location/{{$booth_number}}" class="btn btn-primary btn-{{$booth_number}}">{{$booth_number}}</button>
-                @endif
+            @if (in_array($booth_number,array_column($other_booths_array,'booth_map_id')))
+            @php
+            $this_booth = $other_booths->where("booth_map_id","=",$booth_numbers)->first();
+            @endphp
+
+            <button type="button" disabled class="btn btn-secondary btn-{{$booth_number}}"
+                title="{{$this_booth->user?$this_booth->user->name:""}}">{{$booth_number}}</button>
+            @else
+            <button type="submit" formaction="/admin/booth/booth-location/{{$booth_number}}"
+                class="btn btn-primary btn-{{$booth_number}}">{{$booth_number}}</button>
+            @endif
             @endforeach
         </div>
 
--- resources/views/admin/booths/create.blade.php
+++ resources/views/admin/booths/create.blade.php
@@ -11,100 +11,105 @@
 </section>
 
 <section class="content container-fluid">
-  <!--------------------------
+    <!--------------------------
 | Your Page Content Here |
 -------------------------->
 
-<div class="col-xs-12">
-  <div class="box">
+    <div class="col-xs-12">
+        <div class="box">
 
-    <!-- box-header -->
-    <div class="box-header">
-      <h3 class="box-title">{{ __('words.YourBoothInfo') }}</h3>
-    </div>
-    <!-- /.box-header -->
-
-    
-    <form role="form" method="post"  action="{{ url('admin/booth') }}" enctype="multipart/form-data">
-      @csrf
-
-      <input name="package_id" id="package_id" type="hidden" value="{{$package_id}}" />
-      <input name="expo_id" id="expo_id" type="hidden" value="{{$expo_id}}" />
-
-      <div class="box-body">
-        <div asp-validation-summary="ModelOnly" class="text-danger"></div>
-
-        <div class="form-group">
-          <label for="Name" class="control-label">{{ __('words.Pic') }}</label>
-          <div>
-            <img id="inputImage" onclick="$('#pic').trigger('click');" style="cursor: pointer;width: auto;height: 180px;"
-              src="/img/no-image.png" class="img-rounded" alt="no Image Available">
-            <input id="pic" name="pic" type="file" onchange="GetImage()" style="display: none" accept="image/*"/>
-          </div>
-        </div>
-
-        <div class="form-group">
-          <label for="title" class="control-label">{{ __('words.Title') }}</label>
-          <input id="title" name="title" class="form-control"/>
-          <span for="title" class="text-danger"></span>
-        </div>
-
-        <div class="form-group">
-          <label for="description" class="control-label">{{ __('words.Description') }}</label>
-          <textarea id="description" name="description" class="form-control" placeholder="description ..." ></textarea>
-          <span for="description" class="text-danger"></span>
-        </div>
-
-        <div class="form-group">
-          <label for="video" class="control-label">{{ __('words.Video') }}</label>
-          <input id="video" name="video" type="file" accept="video/*" class="form-control-file"/>
-          <span for="video" class="text-danger"></span>
-        </div>
-
-        <div class="form-group">
-          <label for="catalog" class="control-label">{{ __('words.Catalog') }}</label>
-          <input id="catalog" name="catalog" type="file" accept="application/pdf" class="form-control-file"/>
-          <span for="catalog" class="text-danger"></span>
-        </div>
-
-        <div class="form-group">
-          <label for="images" class="control-label">{{ __('words.Images') }}</label>
-          <input id="images" name="images[]" type="file" multiple class="form-control-file"/>
-          <span for="images" class="text-danger"></span>
-        </div>
-
-        <hr>
-
-        @foreach ($themes as $theme)
-
-          <label class="expo-label">
-            <input id="theme_id" name="theme_id" type="radio" value="{{$theme->id}}" required/>
-            <div>
-              <img src="{{$theme->pic}}" alt="{{$theme->title}}" />
-              <h4>{{$theme->title}}</h4>
+            <!-- box-header -->
+            <div class="box-header">
+                <h3 class="box-title">{{ __('words.YourBoothInfo') }}</h3>
             </div>
-          </label>
+            <!-- /.box-header -->
 
-        @endforeach
 
-      </div>
+            <form role="form" method="post" action="{{ url('admin/booth') }}" enctype="multipart/form-data">
+                @csrf
 
-      <!-- /.box-body -->
-      <div class="box-footer">
-        <button type="submit" class="btn btn-primary"><i class="fa fa-save"></i> {{ __('words.Save') }}</button>
-      </div>
+                <input name="package_id" id="package_id" type="hidden" value="{{$package_id}}" />
+                <input name="expo_id" id="expo_id" type="hidden" value="{{$expo_id}}" />
 
-    </form>
+                <div class="box-body">
+                    <div asp-validation-summary="ModelOnly" class="text-danger"></div>
 
-  </div>
-</div>
+                    <div class="form-group">
+                        <label for="Name" class="control-label">{{ __('words.Pic') }}</label>
+                        <div>
+                            <img id="inputImage" onclick="$('#pic').trigger('click');"
+                                style="cursor: pointer;width: auto;height: 180px;" src="/img/no-image.png"
+                                class="img-rounded" alt="no Image Available">
+                            <input id="pic" name="pic" type="file" onchange="GetImage()" style="display: none"
+                                accept="image/*" />
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="title" class="control-label">{{ __('words.Title') }}</label>
+                        <input id="title" name="title" class="form-control" required />
+                        <span for="title" class="text-danger"></span>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="description" class="control-label">{{ __('words.Description') }}</label>
+                        <textarea id="description" name="description" class="form-control" placeholder="description ..."
+                            required></textarea>
+                        <span for="description" class="text-danger"></span>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="video" class="control-label">{{ __('words.Video') }}</label>
+                        <input id="video" name="video" type="file" accept="video/*" class="form-control-file" />
+                        <span for="video" class="text-danger"></span>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="catalog" class="control-label">{{ __('words.Catalog') }}</label>
+                        <input id="catalog" name="catalog" type="file" accept="application/pdf"
+                            class="form-control-file" />
+                        <span for="catalog" class="text-danger"></span>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="images" class="control-label">{{ __('words.Images') }}</label>
+                        <input id="images" name="images[]" type="file" multiple class="form-control-file" />
+                        <span for="images" class="text-danger"></span>
+                    </div>
+
+                    <hr>
+
+                    @foreach ($themes as $theme)
+
+                    <label class="expo-label">
+                        <input id="theme_id" name="theme_id" type="radio" value="{{$theme->id}}" required />
+                        <div>
+                            <img src="{{$theme->pic}}" alt="{{$theme->title}}" />
+                            <h4>{{$theme->title}}</h4>
+                        </div>
+                    </label>
+
+                    @endforeach
+
+                </div>
+
+                <!-- /.box-body -->
+                <div class="box-footer">
+                    <button type="submit" class="btn btn-primary"><i class="fa fa-save"></i>
+                        {{ __('words.Save') }}</button>
+                </div>
+
+            </form>
+
+        </div>
+    </div>
 
 </section>
 
 @endsection
 
 @section('myfooter')
-  <script>
+<script>
     function GetImage() {
       try {
         var input = document.getElementById("pic");
@@ -126,5 +131,5 @@
         showAppMessage(e.statusMessage, "error");
       }
     };
-  </script>
+</script>
 @endsection
